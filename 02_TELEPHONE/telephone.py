8d1fc04c8f826ca958e9944861f3ec21bc298414
Anthony Dickson <eight0153@gmail.com>
2018-01-10 10:58:50 +1300

    Add more program structure

--- 02_TELEPHONE/telephone.py
+++ 02_TELEPHONE/telephone.py
@@ -1,7 +1,31 @@
 import fileinput
 
+telephoneNums = []
+
+def isValid(line):
+    return True
+
+def isDupe(line):
+    return True
+
+def standardFormat(line):
+    return line
+
+def duplicateFormat(line):
+    return standardFormat(line) + "DUP"
+
+def invalidFormat(line):
+    return line + "INV"
+
 def process(line):
-    print line
+    telephoneNums.push(line)
+    
+    if (isValid(line)):
+        print standardFormat(line)
+    elif (isDupe(line)):
+        print duplicateFormat(line)
+    else:
+        print invalidFormat(line)
 
 def main():
     for line in fileinput.input():
