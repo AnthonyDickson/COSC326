475e7d56b83e42f407b8c894ee9002c07d22df7a
Anthony Dickson <eight0153@gmail.com>
2018-01-10 16:23:51 +1300

    Handle duplicate items

--- 02_TELEPHONE/telephone.py
+++ 02_TELEPHONE/telephone.py
@@ -1,36 +1,70 @@
 import fileinput
 
-telephoneNums = []
+class TelephoneNumber:
+    """Represents a telephone number and handles validation and 
+    formatting.
+    """
 
-def isValid(line):
-    return True
+    phonebook = set() # Keeps track of original phone numbers.
 
-def isDupe(line):
-    return True
+    def __init__(self, ph_num):
+        self.original = ph_num.rstrip('\n') # strip trailing carriage return
+        self.formatted = ''
+        self.area_code = 0 # Area code part of the phone number
+        self.number = 0 # The rest of the phone number
 
-def standardFormat(line):
-    return line
+        self.process()
+   
+    def process(self):
+        """Pocess phone number"""        
+        if (self.isValid()):
+            if (self.isDupe()):
+                self.formatted = self.duplicate_format()
+            else:
+                self.formatted = self.standard_format()
+        else:
+            self.formatted = self.invalid_format()
 
-def duplicateFormat(line):
-    return standardFormat(line) + "DUP"
+        TelephoneNumber.phonebook.add(self.original)
 
-def invalidFormat(line):
-    return line + "INV"
+    def isValid(self):
+        """Check if phone number is valid and return True if valid, false
+        otherwise.
+        """
+        return True
 
-def process(line):
-    telephoneNums.push(line)
-    
-    if (isValid(line)):
-        if (isDupe(line)):
-            print duplicateFormat(line)
-        print standardFormat(line)
-    else:
-        print invalidFormat(line)
+    def isDupe(self):
+        """Check if phone number is a duplicate (already processed) 
+        and return True if duplicate, False otherwise.
+        """
+        return self.original in TelephoneNumber.phonebook
+
+    def standard_format(self):
+        """Return phone number in the standard format."""
+        return self.original
+
+    def duplicate_format(self):
+        """Return phone number in the format for duplicate entries."""
+        return self.standard_format() + ' DUP'
+
+    def invalid_format(self):
+        """Return phone number in the format for invalid entries."""
+        return self.original + ' INV'
+
+    def __str__(self):
+        return self.formatted
 
 def main():
+    """Read a file of telephone numbers from stdin and process it 
+    line-by-line.
+    """
+    telephone_numbers = []
+
     for line in fileinput.input():
-        process(line)
+        telephone_numbers.append(TelephoneNumber(line))
     
+    for tn in telephone_numbers:
+        print (tn)
 
 if __name__ == "__main__":
     main()
