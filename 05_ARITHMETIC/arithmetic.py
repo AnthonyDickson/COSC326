f1d2fb1dc0474863280b473bea2d7dcc27aa25d0
Anthony Dickson <eight0153@gmail.com>
2018-01-13 12:13:21 +1300

    Add basic program structure and unit tests.

--- 05_ARITHMETIC/arithmetic.py
+++ 05_ARITHMETIC/arithmetic.py
@@ -1,3 +1,34 @@
+"""arithmetic.py
+Given a set of numbers to use, a target value, and the order to use;
+try find a combination of addition and multiplication operations
+that once performed upon the given numbers, results in the target number.
+
+Author: Anthony Dickson
+Date: 13 January 2018
+"""
+
+class Arithmetic:
+    """Given a set of numbers to use, a target value, and the order to use;
+    try find a combination of addition and multiplication operations
+    that once performed upon the given numbers, results in the target number.
+    """
+    @staticmethod
+    def parse_nums(nums):
+        """Parse a string of space-separated numbers and return the 
+        array of numbers.
+        """
+        return nums
+
+    def __init__(self, nums, target):
+        self.nums = self.parse_nums(nums)
+        self.target = target.split()[0]
+        self.order = target.split()[1]
+
+
+    def solve(self):
+        """Solve the problem. """
+        return ''
+
 def main():
     pass
 
--- 05_ARITHMETIC/test_arithmetic.py
+++ 05_ARITHMETIC/test_arithmetic.py
@@ -1,11 +1,22 @@
 import unittest
+from arithmetic import Arithmetic
 
 class ArithmeticTests(unittest.TestCase):
     """Tests for `arithmetic.py`."""
     
-    def test_true_is_true(self):
-        """Check if unittests is working."""
-        self.assertTrue(True)
+    def test_parse_input(self):
+        """Is input parsed correctly?"""
+        self.assertEqual(Arithmetic('1 2 3', '7 N').nums, [1, 2, 3])
+    
+    def test_parse_target(self):
+        """Is the target number parsed correctly?"""
+        self.assertEqual(Arithmetic('1 2 3', '7 N').target, 7)
+    
+    def test_ouput_is_valid(self):
+        """Is the program output valid?"""
+        self.assertEqual(Arithmetic('1 2 3', '7 N').solve(), 'N 1 + 2 * 3')
+        self.assertEqual(Arithmetic('1 2 3', '9 L').solve(), 'L 1 + 2 * 3')
+        self.assertEqual(Arithmetic('1 2 3', '100 N'.solve()), 'N impossible')
 
 if __name__ == '__main__':
     unittest.main()
