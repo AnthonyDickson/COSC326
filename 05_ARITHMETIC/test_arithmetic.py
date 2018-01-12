a7ecc308bd20cae19e4849213ca187ba51eb533f
Anthony Dickson <eight0153@gmail.com>
2018-01-13 11:49:13 +1300

    Initial commit for Etude 5, Arithmetic.

---
+++ 05_ARITHMETIC/test_arithmetic.py
@@ -0,0 +1,11 @@
+import unittest
+
+class ArithmeticTests(unittest.TestCase):
+    """Tests for `arithmetic.py`."""
+    
+    def test_true_is_true(self):
+        """Check if unittests is working."""
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
