7e48e3833845c5b548f7b0178b10f5a96833b880
Anthony Dickson <eight0153@gmail.com>
2018-01-13 18:21:06 +1300

    Implement brute force approach.

--- 05_ARITHMETIC/arithmetic.py
+++ 05_ARITHMETIC/arithmetic.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python
+
 """arithmetic.py
 Application module for the Arithmetic class. 
 
 Read in a sequence of scenarios and print the solution. A scenario 
 should consist of the numbers to use (e.g. 1 2 3) on one line, with each number
 separated by a space; and the target number plus a letter representing which 
-order to use (e.g. 7 N where N=normal order, 9 L where L=left to right order)
-on the next line, also separated with spaces. 
+order to use (e.g. 7 N where N=normal order, 9 L where L=left to right order, 
+if the order is not specified left to right order is assumed) on the next line, 
+also separated with spaces. 
 
 Author: Anthony Dickson
 Date: 13 January 2018
 """
+
+import re
 
 class Arithmetic:
     """Given a set of numbers to use, a target value, and the order to use;
@@ -35,25 +40,126 @@ class Arithmetic:
     @staticmethod
     def parse_nums(nums):
         """Parse a string of space-separated numbers and return the 
-        list of numbers.
+        numbers as a list of integers.
         """
         return list(map(int, nums.split()))
 
     def __init__(self, nums, target):
-        self.nums = self.parse_nums(nums)
-        self.target, self.order = target.split()
-        self.target = int(self.target)
-        self.solution = None
+        if not re.match("^[\d+\s]*\d+$", nums):
+            raise ValueError(repr(nums) + ' is invalid input.')
 
-        self.solve()
+        self.nums = Arithmetic.parse_nums(nums)
+
+        if re.match("^\d+\s[NL]$", target):
+            self.target, self.order = target.split()
+            self.target = int(self.target)
+        elif re.match("^\d+$", target):
+            self.target = int(target)
+            self.order = 'L'
+        else:
+            raise ValueError(repr(target) + ' is invalid input.')
+        
+        self.ops = []
 
     def solve(self):
-        """Solve the problem. """
+        """Solve the problem and return the solution."""
+        # Try all ops equal to '+'.
+        self.ops = ['+'] * len(self.nums)
+        
+        if self.is_solution():
+            return self.get_solution()
+            
+        # Try all ops equal to '*'.
+        self.ops = ['*'] * len(self.nums)
+        
+        if self.is_solution() == self.target:
+            return self.get_solution()
+        
+        # Reset ops to all '+' and...
+        self.ops = ['+'] * len(self.nums)
+
+        # try all other permetations.
+        for i in range(1, len(self.ops)):
+            for j in range(i, len(self.ops)):
+                # Try changing the current op to a '*'.
+                self.ops[j] = '*'
+                
+                if self.is_solution():
+                    return self.get_solution()
+                
+                # Try this permetation with the ops flipped
+                self.flip_ops()
+
+                if self.is_solution():
+                    return self.get_solution()
+
+                # Flip ops back and...
+                self.flip_ops()
+                # and reset the current op.
+                self.ops[j] = '+'
+
+            # Try changing a '+' to a '*'
+            self.ops[i] = '*'
+
         return self.order + ' impossible'
+
+    def is_solution(self):
+        """Return True if the current solution (nums and ops) produces the target
+        value, False otherwise.
+        """
+        if self.order == 'L':
+            return self.compute() == self.target
+
+        return eval(self.merge_nums_ops()) == self.target
+
+    def flip_ops(self):
+        """Flip all '+' ops to '*' ops in self.ops and vice versa.
+
+        The op in position 0 will always be '+' (having the first
+        op as '*' is invalid).
+        
+        >>> a = Arithmetic('1 2 3', '6 N')
+        >>> a.ops = ['+', '*', '+']
+        >>> a.flip_ops()
+        >>> a.ops
+        ['+', '+', '*']
+        """
+        ops = list(map(lambda op: '+' if op == '*' else '*', self.ops))
+        ops[0] = '+'
+        self.ops = ops
+
+    def compute(self, i=0, a=0):
+        """Use nums and ops to compute the result using left to right ordering 
+        and return the result.
+        
+        @param i The index used for recursion.
+        @param a The running total used for recursion.
+        """
+        if i == len(self.nums):
+            return a
+
+        if self.ops[i] == '+':
+            return self.compute(i + 1, a + self.nums[i])
+
+        return self.compute(i + 1, a * self.nums[i])
+
+    def get_solution(self):
+        """Return the solution as a string."""
+        return self.order + ' ' +  self.merge_nums_ops()
+
+    def merge_nums_ops(self):
+        """Merge, or zip, nums and ops and return as string."""
+        result = str(self.nums[0])
+
+        for i in range(1, len(self.nums)):
+            result += ' ' + str(self.ops[i]) + ' ' + str(self.nums[i])
+
+        return result
 
     def __str__(self):
         """Return the solution as a string."""
-        return self.solution
+
+        return self.get_solution()
 
 def main():
     pass
--- 05_ARITHMETIC/test_arithmetic.py
+++ 05_ARITHMETIC/test_arithmetic.py
@@ -3,7 +3,7 @@ from arithmetic import Arithmetic
 
 class ArithmeticTests(unittest.TestCase):
     """Tests for `arithmetic.py`."""
-    
+
     def test_parse_input(self):
         """Is input parsed correctly?"""
         self.assertEqual(Arithmetic('1 2 3', '7 N').nums, [1, 2, 3])
@@ -16,6 +16,56 @@ class ArithmeticTests(unittest.TestCase):
         """Is the order parsed correctly?"""
         self.assertEqual(Arithmetic('1 2 3', '7 N').order, 'N')
         self.assertEqual(Arithmetic('1 2 3', '9 L').order, 'L')
+        self.assertEqual(Arithmetic('1 2 3', '9').order, 'L')
+
+    def test_flip_ops(self):
+        """Are ops flipped correctly?"""
+        a = Arithmetic('1 2 3', '6 N')
+        a.ops = ['+', '*', '+']
+        a.flip_ops()
+        self.assertEqual(a.ops, ['+', '+', '*'])
+
+    def test_compute(self):
+        """Does compute work correctly?"""
+        a = Arithmetic('1 2 3', '7 N')
+        a.ops = ['+'] * 3
+        self.assertEqual(a.compute(), 6)
+
+        a.ops = ['+', '+', '*']
+        self.assertEqual(a.compute(), 9)
+
+        a.ops = ['+', '*', '+']
+        self.assertEqual(a.compute(), 5)
+
+        a.ops = ['+', '*', '*']
+        self.assertEqual(a.compute(), 6)
+
+    def test_string_formatting(self):
+        """Do the string formatting functions work correctly?"""
+        a = Arithmetic('1 2 3', '6 L')
+        a.ops = ['+'] * 3
+        self.assertEqual(a.merge_nums_ops(), '1 + 2 + 3')
+        self.assertEqual(a.get_solution(), 'L 1 + 2 + 3')
+
+    def test_solve_N(self):
+        """Are N ordered problems solved correctly?"""
+        self.assertEqual(Arithmetic('1 2 3', '6 N').solve(), 'N 1 + 2 + 3')
+        self.assertEqual(Arithmetic('1 2 3', '5 N').solve(), 'N 1 * 2 + 3')
+        self.assertEqual(Arithmetic('1 2 3', '7 N').solve(), 'N 1 + 2 * 3')
+        self.assertEqual(Arithmetic('1 2 3 4', '10 N').solve(), 'N 1 + 2 + 3 + 4')
+        self.assertEqual(Arithmetic('1 2 3 4', '9 N').solve(), 'N 1 * 2 + 3 + 4')
+        self.assertEqual(Arithmetic('1 2 3 4', '11 N').solve(), 'N 1 + 2 * 3 + 4')
+        self.assertEqual(Arithmetic('1 2 3 4', '15 N').solve(), 'N 1 + 2 + 3 * 4')
+        self.assertEqual(Arithmetic('1 2 3 4', '14 N').solve(), 'N 1 * 2 + 3 * 4')
+        self.assertEqual(Arithmetic('1 2 3 4', '24 N').solve(), 'N 1 * 2 * 3 * 4')
+        self.assertEqual(Arithmetic('1 2 3 4 5', '30 N').solve(), 'N 1 + 2 * 3 * 4 + 5')
+
+    def test_solve_L(self):
+        """Are L ordered problems solved correctly?"""
+        self.assertEqual(Arithmetic('1 2 3', '6 L').solve(), 'L 1 + 2 + 3')
+        self.assertEqual(Arithmetic('1 2 3', '5 L').solve(), 'L 1 * 2 + 3')
+        self.assertEqual(Arithmetic('1 2 3', '9 L').solve(), 'L 1 + 2 * 3')
+        self.assertEqual(Arithmetic('1 2 3 4', '36 L').solve(), 'L 1 + 2 * 3 * 4')
     
     def test_ouput_is_valid(self):
         """Is the program output valid?"""
@@ -24,4 +74,4 @@ class ArithmeticTests(unittest.TestCase):
         self.assertEqual(Arithmetic('1 2 3', '100 N').solve(), 'N impossible')
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=1)
