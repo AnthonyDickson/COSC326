89cae45cdda2c185a304e571dd10c592c68f5104
Anthony Dickson <eight0153@gmail.com>
2018-01-17 16:48:06 +1300

    Finish up recursive DFS.

--- 05_ARITHMETIC/arithmetic.py
+++ 05_ARITHMETIC/arithmetic.py
@@ -13,6 +13,7 @@ also separated with spaces.
 Author: Anthony Dickson
 Date: 13 January 2018
 """
+
 import fileinput
 
 class Arithmetic:
@@ -36,26 +37,44 @@ class Arithmetic:
     >>> Arithmetic('1 2 3', '100 N')
     N impossible
     """
-    DEBUG = False
 
     def __init__(self, nums, target):        
         self.nums = list(map(int, nums.split()))
         self.target, self.order = target.split()
         self.target = int(self.target)        
-        self.ops = []
         self.target_depth = len(self.nums) - 1
 
     def solve(self):
         """Try to find a solution and return it."""
+        self.ops = []
+
         if self.search():
             return str(self)
         
         return self.order + ' impossible'
 
-    def search(self, value=0, pending=0):
-        """Perform a depth-first search."""
-        depth = len(self.ops)
+    def search(self, depth=0, value=0, pending=0):
+        """Perform a depth-first search and try to find the solution."""
+        value, pending = self.compute(depth, value, pending)
+
+        if depth == self.target_depth:
+            return value + pending == self.target
         
+        if value + pending >= self.target:
+            return False
+        else:
+            for op in '*+':
+                self.ops.append(op)   
+
+                if self.search(depth + 1, value, pending):
+                    return True
+
+                self.ops.pop()
+
+            return False  
+
+    def compute(self, depth, value, pending):
+        """Compute and return the new values for value and pending."""
         if self.order == 'L':
             if self.ops == []:
                 value = self.nums[depth]
@@ -72,43 +91,14 @@ class Arithmetic:
                 value += pending
                 pending = self.nums[depth]
 
-        if depth == self.target_depth:
-            if value + pending == self.target:
-                return True
-            else:
-                # This path done.
-                self.ops.pop()
-                return False
-        
-        
-        if value + pending >= self.target:
-            # This node done.
-            self.ops.pop()
-            return False
-        else:
-            # Search left.
-            self.ops.append('*')            
-            if self.search(value, pending):
-                return True
-
-            # Search right.
-            self.ops.append('+')
-            if self.search(value, pending):
-                return True
-
-            # This node done.
-            if len(self.ops) > 0:
-                self.ops.pop()
-
-            return False         
+        return value, pending
             
-
     def __str__(self):
         """Return the solution as a string."""
         result = self.order + ' ' + str(self.nums[0])
 
-        for i, op in enumerate(self.ops):
-            result += ' ' + op + ' ' + str(self.nums[i + 1])
+        for op, num in zip(self.ops, self.nums[1:]):
+            result += ' ' + op + ' ' + str(num)
 
         return result
 
