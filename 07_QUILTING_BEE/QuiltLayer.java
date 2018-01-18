b5f2b6fc9ee9e81f25d820f9be56fbb478104cd4
Anthony Dickson <eight0153@gmail.com>
2018-01-18 17:20:31 +1300

    Make first layer be centered on the screen. Make layers after the first now show 4 shapes.

--- 07_QUILTING_BEE/QuiltLayer.java
+++ 07_QUILTING_BEE/QuiltLayer.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 
 /**  A quilt layer. */
 public class QuiltLayer extends JPanel {
+    /** Default values for position and size. */
     static final int RECT_X = 20;
     static final int RECT_Y = RECT_X;
     static final int RECT_WIDTH = 100;
@@ -12,22 +13,38 @@ public class QuiltLayer extends JPanel {
     
     public float scale;
     public int r, g, b;
+    public Point pos;
+    public Dimension size;
     
     public QuiltLayer() {
       super();
+
+      pos = new Point(RECT_X, RECT_Y);
+      size = new Dimension(RECT_WIDTH, RECT_HEIGHT); 
+    }
+
+    /** Copy constructor. */
+    public QuiltLayer(QuiltLayer other) {
+        this.scale = other.scale;
+        this.r = other.r;
+        this.g = other.g;
+        this.b = other.b;
+        this.pos = new Point(other.pos);
+        this.size = new Dimension(other.size);
     }
     
     @Override
     public void paintComponent(Graphics g) {
       super.paintComponent(g);
+
       g.setColor(new Color(this.r, this.g, this.b));
-      g.fillRect((int) (scale * RECT_X), (int) (scale * RECT_Y), (int) (scale * RECT_WIDTH), (int) (scale *RECT_HEIGHT));
+      g.fillRect(pos.x, pos.y, size.width, size.height);
     }
     
-    @Override
-    public Dimension getPreferredSize() {
-      return new Dimension((int) (scale * (RECT_WIDTH + 2 * RECT_X)), (int) (scale * (RECT_HEIGHT + 2 * RECT_Y)));
-    }
+    // @Override
+    // public Dimension getPreferredSize() {
+    //   return new Dimension(size.width + 2 * pos.x, size.height + 2 * pos.y);
+    // }
 
     public String toString() {
         return String.format("[%f, %d, %d, %d]", scale, r, g, b);
--- 07_QUILTING_BEE/QuiltingBeeApp.java
+++ 07_QUILTING_BEE/QuiltingBeeApp.java
@@ -3,18 +3,24 @@ package quiltingbee;
 import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.*;
+import java.awt.Dimension;
 
 /**
  * QuiltingBeeApp
  */
 public class QuiltingBeeApp extends JPanel {   
+    static final Dimension SCREEN_SIZE = new Dimension(800, 600); // Screen width and height in pixels.
   
     private static void display(QuiltLayer[] layers) {
+        QuiltingBee quilt = new QuiltingBee(layers, SCREEN_SIZE);
+        
+        System.out.println(quilt);
+        
         JFrame frame = new JFrame("Quilting Bee");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        QuiltingBee quilt = new QuiltingBee(layers);        
         frame.getContentPane().add(quilt);
         frame.pack();
+        frame.setSize(SCREEN_SIZE);
         frame.setVisible(true);
     }
 
@@ -26,6 +32,7 @@ public class QuiltingBeeApp extends JPanel {
         while (in.hasNextLine()) {
             line = new Scanner(in.nextLine());
             QuiltLayer layer = new QuiltLayer();
+
             layer.scale = line.nextFloat();
             layer.r = line.nextInt();
             layer.g = line.nextInt();
