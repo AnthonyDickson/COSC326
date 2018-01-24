3b3799bf36c059dd831288b29b0c81fd7b5ed36e
Anthony Dickson <eight0153@gmail.com>
2018-01-25 11:04:28 +1300

    Update Identity.java

--- 06_NUMBERS/src/numbers/Identity.java
+++ 06_NUMBERS/src/numbers/Identity.java
@@ -3,46 +3,82 @@ package numbers;
 import java.util.*;
 
 public class Identity{
-  /**
-   * \brief   A Constructor to handle initialisation of member variables.
-   */
-  public Identity(){
-
-  }
-
-  public static float calculate(float x,float y){
-    return (( (x/y) - (x*y) )*y + (x*y*y));
-  }
-
-  public static double calculate(double x,double y){
-    return (( (x/y) - (x*y) )*y + (x*y*y));
-  }
-
-  public static void main(String args[]){
-    String str = "float";
-    float sX=2.54f,sY=3.5f;
-    double dX=2.5848000000,dY=3.5234000000;
-    if(args.length > 0){
-      if(str.equals(args[0])){
-        if(args.length > 1){
-          sX = Float.parseFloat(args[1]);
-        }
-        if(args.length > 2){
-          sY = Float.parseFloat(args[2]);
-        }
-      } else {
-        if(args.length > 1){
-          dX = Double.parseDouble(args[1]);
-        }
-        if(args.length > 2){
-          dY = Double.parseDouble(args[2]);
-        }
-      }
+    public static Random rand = new Random();
+    public static double MAX_ERROR = 0.5;
+    
+    public static float calculate(float x,float y){
+        return (((x / y) - (x * y)) * y + (x * y * y));
     }
-    Identity id = new Identity();
-    System.out.println("Single Precision Input x = "+ sX + "  y = "+ sY);
-    System.out.println("Single Precision Left Side: " + sX + "  Right Side: " + id.calculate(sX,sY) + "  Result: " + (sX==id.calculate(sX,sY)));
-    System.out.println("Double Precision Input x = "+ dX + "  y = "+ dY);
-    System.out.println("Double Precision Left Side: " + dX + "  Right Side: " + id.calculate(dX,dY) + "  Result: " + (dX==id.calculate(dX,dY)));
-  }
+
+    public static double calculate(double x,double y){
+        return (((x / y) - (x * y)) * y + (x * y * y));
+    }
+
+    public static void testSP(float x, float y, int range) {
+        System.out.println("\nSingle Precision:");
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+               
+        float LHS = x;
+        float RHS = calculate(x, y);
+        double percentError = 100 * Math.abs(LHS - RHS) / ((Math.abs(LHS) + Math.abs(RHS)) / 2);
+        
+        System.out.println("LHS: " + LHS);
+        System.out.println("RHS: " + RHS);
+        System.out.println(String.format("Percent Error: %.2f%%", percentError));
+        System.out.println("LHS == RHS: " + (percentError < MAX_ERROR));
+    }
+    
+    public static void testDP(double x, double y, int range) {       
+        System.out.println("\nDouble Precision:");
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+               
+        double LHS = x;
+        double RHS = calculate(x, y);
+        double percentError = 100 * Math.abs(LHS - RHS) / (Math.abs(LHS) + Math.abs(RHS) / 2);
+        
+        System.out.println("LHS: " + LHS);
+        System.out.println("RHS: " + RHS);
+        System.out.println(String.format("Percent Error: %.2f%%", percentError));
+        System.out.println("LHS == RHS: " + (percentError < MAX_ERROR));
+    }
+
+    public static void main(String args[]){
+        try {
+            int nTests = Integer.parseInt(args[0]);
+            int range = (args.length > 1) ? Integer.parseInt(args[1]) : 100;
+            double x = 0;
+            double y = 1;
+            int i = 0;
+
+            if (args.length == 4) {
+                x = Double.parseDouble(args[2]);
+                y = Double.parseDouble(args[3]);
+                
+                testSP((float) x, (float) y, range);
+                testDP(x, y, range);
+            }
+            
+            while (i++ < nTests) {
+                System.out.println("################################################################################");
+                System.out.println("Test #" + i);
+                
+                x = rand.nextDouble() * 2 * range - range;
+                y = rand.nextDouble() * 2 * range - range;
+
+                while (y == 0) {
+                    y = rand.nextDouble() * 2 * range - range;
+                }
+                
+                testSP((float) x, (float) y, range);
+                testDP(x, y, range);                
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Usage: java numbers.Identity <# tests> [<range> [<x> <y>]]");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Usage: java numbers.Identity <# tests> [<range> [<x> <y>]]");
+        }
+    }
 }
